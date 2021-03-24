a67f5faee72e74a550373261f77b86094f6ef0bf
Nicolas Iglesias <nicolas.iglesias@clearwatercompliance.com>
2021-03-24 11:26:38 -0300

    - clean default ng view - minor linting fixes

--- server/src/tests/controllers/test.controller.ts
+++ server/src/tests/controllers/test.controller.ts
@@ -1,34 +1,38 @@
-import { Controller, Get, Post, Put, Delete, Body, Param } from  '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { TestsService } from '../services/tests.service';
 import { Test } from '../models/test.entity';
 
-
 @Controller('tests')
 export class TestsController {
-   
-  /**
-   *
-   */
+
   constructor(private testsService: TestsService) {
   }
-  
+
   @Get()
   index(): Promise<Test[]> {
     return this.testsService.findAll();
-  }   
+  }
 
   @Post('create')
   async create(@Body() testData: Test): Promise<any> {
     return this.testsService.create(testData);
-  }  
+  }
 
   @Put(':id/update')
   async update(@Param('id') id, @Body() testData: Test): Promise<any> {
-      testData.id = Number(id);
-      console.log('Update #' + testData.id)
-      return this.testsService.update(testData);
-  }  
-  
+    testData.id = Number(id);
+
+    return this.testsService.update(testData);
+  }
+
   @Delete(':id/delete')
   async delete(@Param('id') id): Promise<any> {
     return this.testsService.delete(id);
